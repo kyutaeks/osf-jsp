f4d3d5713f290a85263bd4cf9aba40315f57de32
lgt8879 <lgt8879@gmail.com>
2019-03-20 17:14:54 +0900

    19.03.20 recommit

--- src/com/osf/test/servlet/PBoardServlet.java
+++ src/com/osf/test/servlet/PBoardServlet.java
@@ -16,46 +16,59 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class PBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static String savePath = "D:\\study\\workspace\\osf-jsp\\WebContent\\upload";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uri = request.getRequestURI();
 		uri = uri.replace("/pboard/", "");
-		if("insert".equals(uri)) {
-			DiskFileItemFactory dfiFactory= new DiskFileItemFactory();
+		if ("insert".equals(uri)) {
+			DiskFileItemFactory dfiFactory = new DiskFileItemFactory();
 			String tmpPath = System.getProperty("java.io.tmpdir");
 			File tmpFile = new File(tmpPath);
 			dfiFactory.setRepository(tmpFile);
 //			dfiFactory.setRepository(new File(System.getProperty("java.io.tmpdir")));	위 3줄을 한줄로 쓴것.
-			dfiFactory.setSizeThreshold(10*1024*1024);
-			
-			
+			dfiFactory.setSizeThreshold(10 * 1024 * 1024);
+
 			ServletFileUpload sfu = new ServletFileUpload(dfiFactory);
-			sfu.setSizeMax(20*1024*1024);
-			sfu.setFileSizeMax(20*1024*1024);
+			sfu.setSizeMax(20 * 1024 * 1024);
+			sfu.setFileSizeMax(20 * 1024 * 1024);
 			try {
 				List<FileItem> fileList = sfu.parseRequest(request);
-				for(int i =0; i<fileList.size();i++) {
-					System.out.println("name : " + fileList.get(i).getFieldName());
-					System.out.println("value : " + fileList.get(i).getString("utf-8"));
-					System.out.println(fileList.get(i).isFormField());
-					
+				for (int i = 0; i < fileList.size(); i++) {
+					FileItem fi = fileList.get(i);
+					if (fi.isFormField()) {
+
+					} else {
+						String fileName = fi.getName();
+						File saveFile = new File(savePath + "\\" + fileName);
+						fi.write(saveFile);
+					}
+//					System.out.println("name : " + fileList.get(i).getFieldName());
+//					System.out.println("value : " + fileList.get(i).getString("utf-8"));
+//					System.out.println(fileList.get(i).isFormField());
+
 				}
 			} catch (FileUploadException e) {
 				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			
-		}else if("upeate".equals(uri)) {
-			
-		}else if("delete".equals(uri)) {
-			
-		}else {
-			
+
+		} else if ("upeate".equals(uri)) {
+
+		} else if ("delete".equals(uri)) {
+
+		} else {
+
 		}
-		
+
 	}
 
 }
